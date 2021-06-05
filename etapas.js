30681a40b654c889cbaa0bcf576f7c63f406965c
messiashub <claudineimessias@hotmail.com>
2021-06-04 23:31:39 -0300

    update

---
+++ etapas.js
@@ -0,0 +1,52 @@
+
+
+let etapas = [
+    {
+        titulo:"VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero:38111,
+                name: "Fulano de tal",
+                partido:"ABC",
+                fotos:[
+                    {url:'senhoraPuff.jpg',legenda:"Vereador",small:false}
+                ]
+            },
+            {
+                numero: 77222,
+                name:"Beltrano da Silva",
+                partido:"DEFG",
+                fotos:[
+                    {url:'seu sirigueijo',legenda:'vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo:"PREFEITO",
+        numeros: 2,
+        candidatos:[
+            {
+                numero:99,
+                name:"Ciclano",
+                partido:'ABC',
+                vice:"Cic",
+                fotos:[
+                    {url:"Lula_molusco.jpg",legenda:"Prefeito"},
+                    {url:"sandy1.png", legenda:"Vice-Prefeito", small:true}
+                ]
+            },
+            {
+                numero:89,
+                name:"Zulano",
+                partido:"QWRT",
+                vice:"Zul",
+                fotos:[
+                    {url:"bob.png",legenda:"Prefeito"},
+                    {url:"Patrick.jpg", legenda:"Vice-Prefeito", small:true}
+                ]
+            }
+        ]
+    }
+]
